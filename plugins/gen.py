9892a36afa3aae9e90ce2c2c6c164c3066fa219e
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-06 03:56:15 +0530

    Update gen.py

--- plugins/gen.py
+++ plugins/gen.py
@@ -4,13 +4,13 @@ from shortener import short_url
 from time import time
 from uuid import uuid4
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.private & filters.command("gen"))
 async def gen(client, message):
     user_id = message.chat.id
     if not Config.TOKEN_TIMEOUT:
-        return None, None
+        return
     user_data = await db.get_user_data(user_id)
     data = user_data.get('data', {})
     expire = data.get('time')
@@ -21,7 +21,7 @@ async def gen(client, message):
             del data['time']
         data['token'] = token
         user_data['data'] = data
-        await db.update_user_data(user_id)
+        await db.update_user_data(user_id, user_data)
     buttons = [InlineKeyboardButton(text="Refresh Token", short_url=f'https://t.me/{Config.BOT_NAME}?start={token}')]
-    text = "Token is expired, refresh your token and try again."
-    return text, buttons
+    text = "Token is expired, refresh your token and try again." if isExpired else "Your token is valid."
+    await message.reply(text=text, reply_markup=InlineKeyboardMarkup([buttons]))
