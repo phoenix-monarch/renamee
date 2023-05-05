4d5a9657fc7e2ff1b121d29a5e4784cf2ba8c7c4
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-06 03:43:17 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -5,47 +5,47 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from helper.database import db
 from config import Config, Txt  
-  
+
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
     user = message.from_user
     await db.add_user(client, message)
-# Get user data from the database
-if len(message.command) > 1:
-    data = await db.get_user_data(user.id)
-    input_token = message.command[1]
-    # Check if user's saved token matches input token
-    if 'token' not in data or data['token'] != input_token:
-        return await message.reply(text='This token has expired. Please renew it using /gen')
-    # Check if user's token has expired
-    if time() - data['time'] > Config.TOKEN_TIMEOUT:
-        return await message.reply(text='Your token has expired. Please generate a new one using /gen')
-else:
-    # Check if user's token has expired
-    data = await db.get_user_data(user.id)
-    if 'token' not in data or time() - data['time'] > Config.TOKEN_TIMEOUT:
-        return await message.reply(text='Your token has expired. Please generate a new one using /gen')
-
-# Refresh user's token and save the current time
-data['token'] = str(uuid4())
-data['time'] = time()
-await db.update_user_data(user.id, data)
-    # Display start message with user's name and inline keyboard
-    button = InlineKeyboardMarkup([[
-        InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
-        ],[
-        InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/kirigayaakash'),
-        InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/kirigaya_asuna')
-        ],[
-        InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
-        InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
-    ]])
-    if Config.START_PIC:
-        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
+    # Get user data from the database
+    if len(message.command) > 1:
+        data = await db.get_user_data(user.id)
+        input_token = message.command[1]
+        # Check if user's saved token matches input token
+        if 'token' not in data or data['token'] != input_token:
+            return await message.reply(text='This token has expired. Please renew it using /gen')
+        # Check if user's token has expired
+        if time() - data['time'] > Config.TOKEN_TIMEOUT:
+            return await message.reply(text='Your token has expired. Please generate a new one using /gen')
     else:
-        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+        # Check if user's token has expired
+        data = await db.get_user_data(user.id)
+        if 'token' not in data or time() - data['time'] > Config.TOKEN_TIMEOUT:
+            return await message.reply(text='Your token has expired. Please generate a new one using /gen')
 
-            
+        # Refresh user's token and save the current time
+        data['token'] = str(uuid4())
+        data['time'] = time()
+        await db.update_user_data(user.id, data)
+
+        # Display start message with user's name and inline keyboard
+        button = InlineKeyboardMarkup([[
+            InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
+        ],[
+            InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/kirigayaakash'),
+            InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/kirigaya_asuna')
+        ],[
+            InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
+            InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
+        ]])
+        if Config.START_PIC:
+            await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
+        else:
+            await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
     data = query.data 
