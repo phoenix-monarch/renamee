17f23e3da4b1ae370bf62699bd67714a7575a91b
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-06 18:09:51 +0530

    Update gen.py

--- plugins/gen.py
+++ plugins/gen.py
@@ -15,17 +15,19 @@ async def gen(client, message):
         user_data = await db.get_user_data(user_id)
         data = user_data.get('data', {})
         expire = data.get('time')
-        isExpired = (expire is None or expire is not None and (time() - expire) > Config.TOKEN_TIMEOUT)
-        if isExpired:
+        if expire is not None and time() - expire < Config.TOKEN_TIMEOUT - 60:
+            # Token is not expired yet
+            text = "Your token is valid."
+        else:
+            # Token is expired or about to expire
             token = data.get('token') or str(uuid4())
-            if expire is not None:
-                del data['time']
             data['token'] = token
+            data['time'] = time()
             user_data['data'] = data
             await db.update_user_data(user_id, user_data)
             url = f'https://t.me/{Config.BOT_NAME}?start={token}'
             shortened_url = shorten_url(url)  # pass the generated URL to the `shorten_url` function
-            text = "Token is expired, refresh your token and try again." if isExpired else "Your token is valid."
+            text = "Token is expired, refresh your token and try again."
         await message.reply(text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Shortened URL", url=shortened_url)]])) # add a button with the shortened URL to the inline keyboard
     except Exception as e:
         print(f"An error occurred while generating token: {e}")
