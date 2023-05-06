8d293cd7f26fda32adc1db55c111a20b72f6ade6
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-06 08:57:05 +0530

    Update gen.py

--- plugins/gen.py
+++ plugins/gen.py
@@ -26,6 +26,6 @@ async def gen(client, message):
             url = f'https://t.me/{Config.BOT_NAME}?start={token}'
             shortened_url = shorten_url(url)  # pass the generated URL to the `shorten_url` function
             text = "Token is expired, refresh your token and try again." if isExpired else "Your token is valid."
-        await message.reply(text=text, reply_markup=InlineKeyboardMarkup([buttons, [InlineKeyboardButton(text="Shortened URL", url=shortened_url)]])) # add a button with the shortened URL to the inline keyboard
+        await message.reply(text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Shortened URL", url=shortened_url)]])) # add a button with the shortened URL to the inline keyboard
     except Exception as e:
         print(f"An error occurred while generating token: {e}")
