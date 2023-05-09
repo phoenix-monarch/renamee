00744c6b36ab0144774a1a55cac7347e7a533f6e
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-10 03:02:15 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,4 +1,5 @@
 import random
+import time 
 from uuid import uuid4
 from time import time
 from pyrogram import Client, filters
@@ -14,19 +15,15 @@ async def start(client, message):
     data = await db.get_user_data(user.id)
     if user.id not in data:
         return await message.reply(text='User not found. Please generate a new token using /gen')
-    # Check if user's token has expired
-    token_data = data[user.id]
-    if 'token' not in token_data or time() - token_data['time'] > Config.TOKEN_TIMEOUT:
-        return await message.reply(text='Your token has expired. Please generate a new one using /gen')
     # Check if user provided an input token
     if len(message.command) > 1:
         input_token = message.command[1].upper()
         # Check if user's saved token matches input token
-        if token_data['token'] != input_token:
+        if data[user.id]['token'] != input_token:
             return await message.reply(text='Invalid token. Please renew it using /gen')
     # Refresh user's token and save the current time
-    token_data['token'] = str(uuid4())
-    token_data['time'] = time()
+    data[user.id]['token'] = str(uuid4())
+    data[user.id]['time'] = int(time.time())
     await db.update_user_data(user.id, data)
     button = InlineKeyboardMarkup([[
         InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
@@ -41,6 +38,7 @@ async def start(client, message):
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
     else:
         await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
