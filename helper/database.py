a9954ace5fc20df5a6cd407a5a1b3f73b7e8195d
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-10 09:34:39 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,8 +2,8 @@ import motor.motor_asyncio
 from config import Config
 from .utils import send_log
 
-class Database:
 
+class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
@@ -11,12 +11,7 @@ class Database:
         self.user_data_col = self.db.user_data
 
     def new_user(self, id):
-        return dict(
-            _id=int(id),
-            file_id=None,
-            caption=None,
-            user_data=[]
-        )
+        return dict(_id=int(id), file_id=None, caption=None, user_data=[])
 
     async def add_user(self, b, m):
         u = m.from_user
@@ -27,7 +22,7 @@ class Database:
             await self.user_data_col.insert_one({"user_id": u.id, "data": {}})
 
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'_id': int(id)})
+        user = await self.col.find_one({"_id": int(id)})
         return bool(user)
 
     async def total_users_count(self):
@@ -39,33 +34,33 @@ class Database:
         return all_users
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'_id': int(user_id)})
+        await self.col.delete_many({"_id": int(user_id)})
         await self.user_data_col.delete_many({"user_id": user_id})
 
     async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.col.update_one({"_id": int(id)}, {"$set": {"file_id": file_id}})
 
     async def get_thumbnail(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
+        user = await self.col.find_one({"_id": int(id)})
+        return user.get("file_id", None)
 
     async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+        await self.col.update_one({"_id": int(id)}, {"$set": {"caption": caption}})
 
     async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
+        user = await self.col.find_one({"_id": int(id)})
+        return user.get("caption", None)
 
-    async def get_user_data(self, user_id, data):
+    async def get_user_data(self, user_id):
         user_data = await self.user_data_col.find_one({"user_id": user_id})
         if user_data is None:
-            user_data = {
-                'token': data[user_id]['token'],
-                'time': data[user_id]['time']
-            }
-        return user_data.get('data', {})
+            return {}
+        return user_data.get("data", {})
 
     async def update_user_data(self, user_id, data):
-        await self.user_data_col.update_one({'user_id': user_id}, {'$set': {'data': data}}, upsert=True)
+        await self.user_data_col.update_one(
+            {"user_id": user_id}, {"$set": {"data": data}}, upsert=True
+        )
+
 
 db = Database(Config.DB_URL, Config.DB_NAME)
