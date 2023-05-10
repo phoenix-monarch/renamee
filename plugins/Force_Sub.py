988a033fe4e35d033b7d27030a8f795179fc7db9
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-10 09:05:45 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -7,11 +7,10 @@ from time import time
 from uuid import uuid4
 from shortener import shorten_url
 
-
-async def checking_access(user_id, user_data):
+async def checking_access(message, user_data):
     if not Config.TOKEN_TIMEOUT:
         return None, None
-    user = await db.col.find_one({'_id': int(user_id)})
+    user = await db.col.find_one({'_id': int(message.from_user.id)})
     if user is None:
         return 'User not found in the database', None
     expire = user_data.get('time')
@@ -19,13 +18,13 @@ async def checking_access(user_id, user_data):
     if is_expired:
         token = user_data.get('token') if (expire is None and 'token' in user_data) else str(uuid4())
         if expire is not None:
-            await db.update_user_data(user_id, {'time': None})
-        await db.update_user_data(user_id, {'token': token})
+            await db.update_user_data(message.from_user.id, {'time': None})
+        await db.update_user_data(message.from_user.id, {'token': token})
         if user_data.get('time') is None:
             user_data.pop('time')
         if user_data.get('token') is None:
             user_data.pop('token')
-        await db.update_user_data(user_id, user_data)
+        await db.update_user_data(message.from_user.id, user_data)
         url = f'https://t.me/{Config.BOT_NAME}?start={token}'
         shortened_url = shorten_url(url)  # pass the generated URL to the shorten_url function
         text = "🤣Here is your wedding ring🤣:"
@@ -37,7 +36,6 @@ async def checking_access(user_id, user_data):
         ) # add a button with the shortened URL to the inline keyboard
     else:
         return None, None
-
 
 async def not_subscribed(_, client, message):
     await db.add_user(client, message)
@@ -53,27 +51,27 @@ async def not_subscribed(_, client, message):
         pass
     return True
 
-
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
     # Check if user token is expired or not
-    error_message, user_data = await checking_access(message.from_user.id, await db.get_user_data(message.from_user.id))
+    error_message, user_data = await checking_access(message, await db.get_user_data(message.from_user.id))
     if error_message is not None:
         return await message.reply_text(error_message)
-
     # If token is expired, restrict bot's access until a new token is set
     while user_data.get('time') is not None and (time() - user_data.get('time')) > Config.TOKEN_TIMEOUT:
         await message.reply_text('Your token has expired. Please set a new token to continue.')
-        await checking_access(message.from_user.id, await db.get_user_data(message.from_user.id))
-        user_data = await db.get_user_data(message.from_user.id)
-    
+        await checking_access(message, await db.get_user_data(message.from_user.id))
+        user_data = await db.get_user_data(message.from_user.id)    
     buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}")]]
     text = "**Sorry Dude, you're not joined my channel 😐. So please join our update channel to continue.**"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
         if user.status == enums.ChatMemberStatus.BANNED:
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
-    except UserNotParticipant:                       
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-           
+            return True
+        else:
+            return False
+    except UserNotParticipant:
+        pass
+    return True
+    except Exception as e:
+        print(f"An error occurred while generating token: {e}")
