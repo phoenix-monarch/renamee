621d7e7d8a89dd08467eae63aa7ef15f7a6d37f2
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-10 09:22:12 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -9,31 +9,34 @@ from config import Config, Txt
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    user = message.from_user
-    await db.add_user(client, message)
-    data = await db.get_user_data(user.id)
-    if user.id not in data:
-        return await message.reply(text='User not found.')
-    if len(message.command) > 1:
-        input_token = message.command[1].upper()
-        if data[user.id]['token'] != input_token:
-            return await message.reply(text='Invalid token.')
-    data[user.id]['token'] = str(uuid4())
-    data[user.id]['time'] = int(time.time())
-    await db.update_user_data(user.id, data)
-    button = InlineKeyboardMarkup([[
-        InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
-    ],[
-        InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/kirigayaakash'),
-        InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/kirigaya_asuna')
-    ],[
-        InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
-        InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
-    ]])
-    if Config.START_PIC:
-        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
-    else:
-        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+    try:
+        user = message.from_user
+        await db.add_user(client, message)
+        data = await db.get_user_data(user.id)
+        if user.id not in data:
+            return await message.reply(text='User not found.')
+        if len(message.command) > 1:
+            input_token = message.command[1].upper()
+            if data[user.id]['token'] != input_token:
+                return await message.reply(text='Invalid token.')
+        data[user.id]['token'] = str(uuid4())
+        data[user.id]['time'] = int(time.time())
+        await db.update_user_data(user.id, data)
+        button = InlineKeyboardMarkup([[
+            InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
+        ],[
+            InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/kirigayaakash'),
+            InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/kirigaya_asuna')
+        ],[
+            InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
+            InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
+        ]])
+        if Config.START_PIC:
+            await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
+        else:
+            await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+    except Exception as e:
+        print(f"An error occurred while generating token: {e}")
 
 
 @Client.on_callback_query()
