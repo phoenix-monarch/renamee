e2c4e9019c51269bd412b4c4678613722f97343f
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-12 01:54:05 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -9,7 +9,7 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.user
+        self.col = self.db.users
         self.user_data_col = self.db.user_data
 
     def new_user(self, id):
