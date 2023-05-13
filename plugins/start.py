54e1e96d10e2010a26474680e4e3d90ba1eea6d1
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-14 02:11:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -59,9 +59,9 @@ async def start(client, message):
             caption=caption,
             supports_streaming=True
         )
-    except Exception as e:
-        LOGGER.error(f"An error occurred while executing: {e}")
-
+    except Exception as e: 
+        print(f"An error occurred while executing start: {e}")
+    
 @Client.on_message(filters.private & filters.command(['ping']))
 async def ping(client, message):
     try:
@@ -75,4 +75,4 @@ async def ping(client, message):
         duration = round((end - start) * 1000, 3)
         await sent_message.edit_text(f"😶😑😏: {duration}ms")
     except Exception as e:
-        LOGGER.error(f"An error occurred while executing: {e}")
+        print(f"An error occurred while executing ping: {e}")
