4c4b76c06d686af052c411620298624bd6056d6d
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-13 16:48:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,6 @@
 import random
 import os
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait, RPCError
 from asyncio import sleep
 from time import time
 from uuid import uuid4
@@ -11,24 +10,6 @@ from config import Config
 from helper.database import db
 from helper.token import checking_access
 
-async def sendMessage(client, message, text):
-    try:
-        return await message.reply_text(
-            text=text,
-            quote=True,
-            disable_web_page_preview=True,
-            disable_notification=True
-        )
-    except FloodWait as f:
-        LOGGER.warning(str(f))
-        await sleep(f.value * 1.2)
-        return await sendMessage(client, message, text)
-    except RPCError as e:
-        LOGGER.error(f"{e.NAME}: {e.MESSAGE}")
-    except Exception as e:
-        LOGGER.error(str(e))
-        print(f"An error occurred while executing: {e}")
-
 @Client.on_message(filters.command(['start']))
 async def start(client, message):
     try:
@@ -36,10 +17,10 @@ async def start(client, message):
             userid = message.from_user.id
             input_token = message.command[1]
             if not await db.is_user_exist(userid):
-                return await sendMessage(client, message, 'wait a minute who are you?')
+                return await Config.sendMessage(client, message, 'wait a minute who are you?')
             data = await db.get_user_data(userid)
             if 'token' not in data or data['token'] != input_token:
-                return await sendMessage(client, message, 'This is a token already expired')
+                return await Config.sendMessage(client, message, 'This is a token already expired')
             await checking_access(userid, message)
             data = await db.get_user_data(userid)
         gifs = os.listdir('./gif')
@@ -50,7 +31,6 @@ async def start(client, message):
             supports_streaming=True
         )
     except Exception as e:
-        LOGGER.error(str(e))
         print(f"An error occurred while executing: {e}")
         
 @Client.on_message(filters.command(['ping']))
