00985e5b1614ceccb0aeee8c9ae0f9dae9b38b4f
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-15 16:44:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,16 +11,15 @@ from time import time
 async def start(client, message):
     try:
         is_valid = await validate_user(client, message)
-        if not is_valid:
-            return
-        gifs = os.listdir('./gif')
-        selected_gif = random.choice(gifs)
-        caption = f'Hello {message.from_user.first_name}! Welcome to the bot'
-        await message.reply_video(
-            video=f'./gif/{selected_gif}',
-            caption=caption,
-            supports_streaming=True
-        )
+        if is_valid:
+            gifs = os.listdir('./gif')
+            selected_gif = random.choice(gifs)
+            caption = f'Hello {message.from_user.first_name}! Welcome to the bot'
+            await message.reply_video(
+                video=f'./gif/{selected_gif}',
+                caption=caption,
+                supports_streaming=True
+            )
     except Exception as e: 
         print(f"An error occurred while executing start: {e}")
     
@@ -28,13 +27,12 @@ async def start(client, message):
 async def ping(client, message):
     try:
         is_valid = await validate_user(client, message)
-        if not is_valid:
-            return
-        start = time()
-        sent_message = await message.reply("😐😑😶")
-        await asyncio.sleep(3)
-        end = time()
-        duration = round((end - start) * 1000, 3)
-        await sent_message.edit_text(f"😶😑😏: {duration}ms")
+        if is_valid:
+            start = time()
+            sent_message = await message.reply("😐😑😶")
+            await asyncio.sleep(3)
+            end = time()
+            duration = round((end - start) * 1000, 3)
+            await sent_message.edit_text(f"😶😑😏: {duration}ms")
     except Exception as e:
         print(f"An error occurred while executing ping: {e}")
