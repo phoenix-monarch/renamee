5f3d291bdf1ba65d08cc0e9f3762b05a72ece9a2
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-15 15:52:54 +0530

    Update token.py

--- helper/token.py
+++ helper/token.py
@@ -8,51 +8,58 @@ from helper.database import db
 from config import Config
 from shortener import shorten_url
 
-try:
-    async def validate_user(client, message):
-        userid = message.from_user.id
-        data = await db.get_user_data(userid)
-        if data is None:
-            data = {}
-            await db.update_user_data(userid, data)
-        input_token = None
-        if Config.TOKEN_TIMEOUT:
-            expire = data.get('time')
-            isExpired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
-            if isExpired:
-                data['token'] = str(uuid4())
-                url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
-                shortened_url = shorten_url(url)
-                button = InlineKeyboardButton(text='Refresh Token', url=shortened_url)
-                await client.send_message(
-                    chat_id=message.chat.id,
-                    text='Your token has expired. Please renew it using the button below.',
-                    reply_markup=InlineKeyboardMarkup([[button]])
-                )
-            if len(message.command) > 1:
-                input_token = message.command[1]
-            while True:
-                if 'token' not in data or data['token'] != input_token:
-                    data['token'] = str(uuid4())
-                    data['time'] = time()
-                    await db.update_user_data(userid, data)
-                else:
-                    break
-        else:
+ async def validate_user(client, message):
+    userid = message.from_user.id
+    data = await db.get_user_data(userid)
+    if data is None:
+        data = {}
+        await db.update_user_data(userid, data)
+    input_token = None
+    if len(message.command) > 1:
+        input_token = message.command[1]
+    
+    if Config.TOKEN_TIMEOUT:
+        expire = data.get('time')
+        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
+        if is_expired:
             data['token'] = str(uuid4())
             data['time'] = time()
-            await db.update_user_data(userid, data)
-        if 'token' not in data:
             url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
-            shortened_url = shorten_url(url)
+            shortened_url = shorten_url
             button = InlineKeyboardButton(text='Refresh Token', url=shortened_url)
+            await db.update_user_data(userid, data)
             await client.send_message(
                 chat_id=message.chat.id,
-                text='Please provide your token. Click the button to renew your token.',
+                text='Your token has expired.',
                 reply_markup=InlineKeyboardMarkup([[button]])
             )
             return False
-        return True
-
-except Exception as e: 
-    print(f"An error occurred while executing validate_user: {e}")
+    if input_token:
+        if 'token' not in data or data['token'] != input_token:
+            data['token'] = str(uuid4())
+            data['time'] = time()
+            await db.update_user_data(userid, data)
+            url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
+            shortened_url = shorten_url
+            button = InlineKeyboardButton(text='Refresh Token', url=shortened_url)
+            await client.send_message(
+                chat_id=message.chat.id,
+                text='Invalid token.',
+                reply_markup=InlineKeyboardMarkup([[button]])
+            )
+            return False
+    else:
+        if 'token' not in data or is_expired:
+            data['token'] = str(uuid4())
+            data['time'] = time()
+            await db.update_user_data(userid, data)
+            url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
+            shortened_url = shorten_url
+            button = InlineKeyboardButton(text='Get Token', url=shortened_url)
+            await client.send_message(
+                chat_id=message.chat.id,
+                text='You need a token to use this bot.',
+                reply_markup=InlineKeyboardMarkup([[button]])
+            )
+            return False
+    return True
