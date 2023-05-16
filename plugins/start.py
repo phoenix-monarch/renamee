4f4b616d0ebcfdd214184e40505dc8c4289e694a
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-16 14:45:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,28 +1,53 @@
-import random, os, asyncio
-from pyrogram import Client, filters
+import os, random, asyncio
 from gif import *
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply, CallbackQuery
+from config import Config
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaAnimation
 from helper.database import db
-from config import Config 
 from helper.token import validate_user
 from time import time
- 
+from uuid import uuid4
+
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     try:
-        is_valid = await validate_user(client, message)
-        if is_valid:
-            gifs = os.listdir('./gif')
-            selected_gif = random.choice(gifs)
-            caption = f'Hello {message.from_user.first_name}! Welcome to the bot'
-            await message.reply_video(
-                video=f'./gif/{selected_gif}',
-                caption=caption,
-                supports_streaming=True
-            )
-    except Exception as e: 
+        if len(message.command) > 1:
+            userid = message.from_user.id
+            input_token = message.command[1]
+            if not await db.is_user_exist(userid):
+                gif_url = 'https://graph.org/file/a58b959cc11443ac4e70b.mp4'
+                caption = 'Who are you?'
+                await message.reply_animation(
+                    animation=gif_url,
+                    caption=caption,
+                    supports_streaming=True
+                )
+                return
+            data = await db.get_user_data(userid)
+            if 'token' not in data or data['token'] != input_token:
+                gif_url = 'https://graph.org/file/f6e6beb62a16a46642fb4.mp4'
+                caption = 'This is a token already expired'
+                await message.reply_animation(
+                    animation=gif_url,
+                    caption=caption,
+                    supports_streaming=True
+                )
+                return
+            data['token'] = str(uuid4())
+            data['time'] = time()
+            await db.update_user_data(userid, data)
+
+        gifs = os.listdir('./gif')
+        selected_gif = random.choice(gifs)
+        caption = f'Hello {message.from_user.first_name}! Welcome to the bot'
+        await message.reply_video(
+            video=f'./gif/{selected_gif}',
+            caption=caption,
+            supports_streaming=True
+        )
+    except Exception as e:
         print(f"An error occurred while executing start: {e}")
-    
+
 @Client.on_message(filters.private & filters.command(['ping']))
 async def ping(client, message):
     try:
