5cf4939727649ede95cd74ebf2dd14efb8dd7bdd
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-16 22:53:54 +0530

    Update token.py

--- helper/token.py
+++ helper/token.py
@@ -9,18 +9,29 @@ from shortener import shorten_url
 async def validate_user(client, message):
     if not Config.TOKEN_TIMEOUT:
         return None
-    userid = message.from_user.id
-    data = await db.get_user_data(userid)
-    expire = data.get('time')
-    is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
-    if is_expired:
-        token = data.get('token') if expire is None and 'token' in data else str(uuid.uuid4())
-        if expire is not None:
-            del data['time']
-        data['token'] = token
-        await db.update_user_data(userid, data)
-        url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
-        shortened_url = await shorten_url(url)
-        button = InlineKeyboardButton(text='Refresh Token', url=shortened_url)
-        return 'Token is expired, refresh your token and try again.', button
+
+    try:
+        userid = message.from_user.id
+        data = await db.get_user_data(userid)
+        expire = data.get('time')
+        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
+        if is_expired:
+            token = data.get('token') if expire is None and 'token' in data else str(uuid.uuid4())
+            if expire is not None:
+                del data['time']
+            data['token'] = token
+            await db.update_user_data(userid, data)
+            url = f'https://t.me/{Config.BOT_NAME}?start={data["token"]}'
+            shortened_url = await shorten_url(url)
+            button = InlineKeyboardButton(text='Refresh Token', url=shortened_url)
+            await client.send_message(
+                chat_id=message.chat.id,
+                text='Invalid token.',
+                reply_markup=InlineKeyboardMarkup([[button]])
+            )
+        else:
+            await asyncio.sleep(5)
+    except Exception as e:
+        print(f"An error occurred while executing validate_user: {e}")
+        
     return None
