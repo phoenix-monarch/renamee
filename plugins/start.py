7d8b65a099362ff881c5e51710f10ee415358b5f
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-20 19:57:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,21 +16,16 @@ async def start(client, message):
         input_token = None
         if len(message.command) > 1:
             input_token = message.command[1]
-        print("Before if statement")
         if not await db.is_user_exist(userid):
-            print("Inside if statement")
             gif_url = 'https://graph.org/file/a58b959cc11443ac4e70b.mp4'
             caption = 'Who are you?'
-            print("Before reply_video")
             await message.reply_video(
                 video=gif_url,
                 caption=caption,
                 supports_streaming=True
             )
-            print("After reply_video")
             return
-        print("After if statement")
-
+ 
         if 'token' not in data or data['token'] != input_token:
             gif_url = 'https://graph.org/file/f6e6beb62a16a46642fb4.mp4'
             caption = '''This token is already expired.
@@ -43,20 +38,28 @@ async def start(client, message):
                 supports_streaming=True
             )
             return
-
+        
+        print("Before update_user_data")
         data['token'] = str(uuid4())
         data['time'] = time()
+        print("Inside db.update_user_data")
         await db.update_user_data(userid, data)
+        print("After update_user_data")
 
+        print("Before page_number")
         page_number = 0
+        print("Before caption")
         caption = get_page_caption(page_number, message.from_user.first_name)
+        print("Before inline_keyboard")
         inline_keyboard = get_inline_keyboard(page_number)
+        print("Before message.reply_video")
         await message.reply_video(
             video=get_page_gif(page_number),
             caption=caption,
             supports_streaming=True,
             reply_markup=InlineKeyboardMarkup(inline_keyboard)
         )
+    print("After reply_video)    
     except Exception as e:
         print(f"An error occurred while executing start: {e}")
 
