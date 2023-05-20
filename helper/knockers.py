f85b5d1bb77a6ad601dd81cd94632be83b0c42e3
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-20 17:29:51 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -5,31 +5,22 @@ async def handle_callback(callback_query: CallbackQuery):
     page_number = 0
     data = callback_query.data
     if data == "previous":
-        page_number = max(page_number - 1, 0)
+        page_number -= 1
     elif data == "next":
-        page_number = min(page_number + 1, 4)
+        page_number += 1
 
     caption = get_page_caption(page_number, callback_query.from_user.first_name)
     inline_keyboard = get_inline_keyboard(page_number)
-    message = callback_query.message
 
-    if message.video:
-        video = message.video
-    else:
+    video = callback_query.message.animation
+    if not video:
         video_path = get_page_gif(page_number)
         video = InputMediaAnimation(media=video_path)
-
-        await message.reply_video(
-            video=video_path,
+        await callback_query.message.edit_video(
+            video=video,
             caption=caption,
             reply_markup=InlineKeyboardMarkup(inline_keyboard)
         )
-        return
-
-    await callback_query.edit_message_video(
-        video=video,
-        caption=caption,
-        reply_markup=InlineKeyboardMarkup(inline_keyboard)
-    )
-
-    await callback_query.edit_message_caption(caption)
+    else:
+        await callback_query.edit_message_caption(caption)
+        await callback_query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(inline_keyboard))
