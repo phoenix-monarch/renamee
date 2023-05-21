8b0070ea8b3f10c1e8053142b658093d340eab10
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-22 04:41:52 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -9,8 +9,13 @@ async def handle_callback(callback_query: CallbackQuery, current_page):
     elif data == "next":
         current_page += 1
 
+    print(f"Current page: {current_page}")
+
     caption = get_page_caption(current_page, callback_query.from_user.first_name)
     inline_keyboard = get_inline_keyboard(current_page)
+
+    print(f"Caption: {caption}")
+    print(f"Inline keyboard: {inline_keyboard}")
 
     try:
         edit_video = not callback_query.message.video
@@ -18,6 +23,7 @@ async def handle_callback(callback_query: CallbackQuery, current_page):
         if edit_video:
             video_path = get_page_gif(current_page)
             video = InputMediaVideo(media=video_path, caption=caption)
+            print(f"Editing media: {video_path}")
             await callback_query.message.edit_media(
                 media=video,
                 reply_markup=InlineKeyboardMarkup(inline_keyboard)
