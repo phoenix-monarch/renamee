c856ab959f745625286bf890f94fb90f488267d9
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-22 12:11:18 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -2,7 +2,7 @@ from pyrogram.types import InputMediaVideo, InputMediaAnimation, CallbackQuery, 
 from helper.bossoms import get_page_gif, get_page_caption, get_inline_keyboard
 
 async def handle_callback(callback_query: CallbackQuery, current_page):
-    data = CallBackQuery.data
+    data = callback_query.data
 
     if data == "previous":
         if current_page[0] == 0:
@@ -15,21 +15,21 @@ async def handle_callback(callback_query: CallbackQuery, current_page):
         else:
             current_page[0] += 1
 
-    caption = get_page_caption(current_page, CallbackQuery.from_user.first_name)
-    inline_keyboard = get_inline_keyboard(current_page)
+    caption = get_page_caption(current_page[0], callback_query.from_user.first_name)
+    inline_keyboard = get_inline_keyboard(current_page[0])
 
     try:
-        edit_video = not CallBackQuery.message.video
+        edit_video = not callback_query.message.video
 
         if edit_video:
-            video_path = get_page_gif(current_page)
+            video_path = get_page_gif(current_page[0])
             video = InputMediaVideo(media=video_path, caption=caption)
-            await CallBackQuery.message.edit_media(
+            await callback_query.message.edit_media(
                 media=video,
                 reply_markup=InlineKeyboardMarkup(inline_keyboard)
             )
         else:
-            await CallBackQuery.message.edit_caption(caption)
-            await CallBackQuery.message.edit_reply_markup(reply_markup=InlineKeyboardMarkup(inline_keyboard))
+            await callback_query.message.edit_caption(caption)
+            await callback_query.message.edit_reply_markup(reply_markup=InlineKeyboardMarkup(inline_keyboard))
     except Exception as e:
         print(f"An error occurred in handle_callback: {e}")
