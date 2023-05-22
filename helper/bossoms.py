15af48467c65654e54042e9f9d5528822d79715b
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-22 22:03:13 +0530

    Update bossoms.py

--- helper/bossoms.py
+++ helper/bossoms.py
@@ -4,12 +4,9 @@ from config import Config
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def get_page_gif(page_number):
-    print("Inside get_page_gif")
     mp4_files = [file for file in os.listdir('./gif') if file.endswith('.mp4')]
     selected_mp4 = random.choice(mp4_files)
     mp4_path = f'./gif/{selected_mp4}'
-    print(f"Page Number: {page_number}")
-    print(f"Selected GIF Path: {mp4_path}")
     return mp4_path
 
 def get_page_caption(page_number, first_name):
