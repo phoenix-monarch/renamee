590ef9fa589831104f5bf8a16e0b7ff94913bc58
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-22 05:41:41 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -4,17 +4,17 @@ from helper.bossoms import get_page_gif, get_page_caption, get_inline_keyboard
 async def handle_callback(callback_query: CallbackQuery, current_page):
     data = callback_query.data
 
-if data == "previous":
-    if current_page[0] == 0:
-        current_page[0] = 0
-    else:
-        current_page[0] -= 1
-elif data == "next":
-    if current_page[0] == 4:
-        current_page[0] = 4
-    else:
-        current_page[0] += 1
-        
+    if data == "previous":
+        if current_page[0] == 0:
+            current_page[0] = 0
+        else:
+            current_page[0] -= 1
+    elif data == "next":
+        if current_page[0] == 4:
+            current_page[0] = 4
+        else:
+            current_page[0] += 1
+
     caption = get_page_caption(current_page, callback_query.from_user.first_name)
     inline_keyboard = get_inline_keyboard(current_page)
 
