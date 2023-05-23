bd327997f9a4442dbf1426b9fc830c849bcd1b56
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-24 03:59:40 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -4,25 +4,18 @@ from helper.bossoms import get_page_caption, get_inline_keyboard, get_page_gif
 async def handle_callback(callback_query: CallbackQuery, current_page):
     data = callback_query.data
 
-    print("Received callback data:", data)
-
     if data == "previous":
         if current_page[0] == 0:
             current_page[0] = 0
         else:
             current_page[0] -= 1
-        print("Previous button clicked. Current page:", current_page[0])
     elif data == "next":
         if current_page[0] == 4:
             current_page[0] = 4
         else:
             current_page[0] += 1
-        print("Next button clicked. Current page:", current_page[0])
-        print("Debug: Inside 'next' section")
 
-    caption = get_page_caption(current_page[0], callback_query.from_user.first_name)
-    print("Current page:", current_page[0])
-    print("Caption:", caption)    
+    caption = get_page_caption(page_number, callback_query.from_user.first_name, callback_query.from_user.last_name, None if not callback_query.from_user.username else '@' + callback_query.from_user.username, callback_query.from_user.mention, callback_query.from_user.id)
     inline_keyboard = get_inline_keyboard(current_page[0])
 
     try:
@@ -37,6 +30,6 @@ async def handle_callback(callback_query: CallbackQuery, current_page):
                 caption,
                 reply_markup=InlineKeyboardMarkup(inline_keyboard)
             )
-            print("Caption and reply markup edited. Caption:", caption)
+            
     except Exception as e:
         print(f"An error occurred in handle_callback: {e}")
