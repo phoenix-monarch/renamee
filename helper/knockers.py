5adba56ab3a1ce085c93763cb59672878f42d735
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-24 21:00:20 +0530

    Update knockers.py

--- helper/knockers.py
+++ helper/knockers.py
@@ -5,22 +5,22 @@ async def handle_callback(callback_query: CallbackQuery, page_number):
     data = callback_query.data
 
     if data == "previous":
-        if page_number == 0:
-            page_number = 0
+        if page_number[0] == 0:
+            page_number[0] = 0
         else:
-            page_number -= 1
+            page_number[0] -= 1
     elif data == "next":
-        if page_number == 4:
-            page_number = 4
+        if page_number[0] == 4:
+            page_number[0] = 4
         else:
-            page_number += 1
+            page_number[0] += 1
 
-    caption = get_page_caption(page_number, callback_query.from_user.first_name, callback_query.from_user.last_name, None if not callback_query.from_user.username else '@' + callback_query.from_user.username, callback_query.from_user.mention, callback_query.from_user.id)
-    inline_keyboard = get_inline_keyboard(page_number)
+    caption = get_page_caption(page_number[0], callback_query.from_user.first_name, callback_query.from_user.last_name, None if not callback_query.from_user.username else '@' + callback_query.from_user.username, callback_query.from_user.mention, callback_query.from_user.id)
+    inline_keyboard = get_inline_keyboard(page_number[0])
 
     try:
         if isinstance(callback_query.message.media, (InputMediaVideo, InputMediaAnimation)):
-            video_path = get_page_gif(page_number)
+            video_path = get_page_gif(page_number[0])
             video = InputMediaVideo(media=video_path, caption=caption)
             await callback_query.message.edit_media(
                 media=video
