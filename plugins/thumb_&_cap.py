75125d790ec42043e3183d7fb8fb9c46d0579396
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-26 20:45:15 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -8,10 +8,12 @@ from helper.token import none_admin_utils
 @Client.on_message(filters.private & filters.command('ping'))
 async def ping(client, message):
     try:
-        none_admin_msg, error_button = await none_admin_utils(message)        
+        none_admin_msg, error_button = await none_admin_utils(message)
+        print(f"none_admin_msg: {none_admin_msg}")
+        print(f"error_button: {error_button}")
         if none_admin_msg:
             error_msg = none_admin_msg
-            reply_markup = InlineKeyboardMarkup([[error_button]]) if error_button else None          
+            reply_markup = InlineKeyboardMarkup([[error_button]]) if not None else None          
             await message.reply_text(
                 text='\n'.join(error_msg),
                 reply_markup=reply_markup
