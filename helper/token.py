cdf0fe1ebdb0f6e4f36653b74aca2bb27fd1dd86
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-26 21:39:13 +0530

    Update token.py

--- helper/token.py
+++ helper/token.py
@@ -12,8 +12,8 @@ async def none_admin_utils(message):
         token_msg, button = await validate_user(message)
         if token_msg is not None:
             error_msg.append(token_msg)
-            error_button = button    
-        return error_msg, error_button
+            error_button = button
+        return error_msg, error_button if error_button else None
     
     except Exception as e:
         print(f"An error occurred in none_admin_utils: {e}")
@@ -22,7 +22,7 @@ async def none_admin_utils(message):
 async def validate_user(message, button=None):
     try:
         if not Config.TOKEN_TIMEOUT:
-            return None, None
+            return None
         userid = message.from_user.id
         data = await db.get_user_data(userid)
         expire = data.get('time')
@@ -39,7 +39,7 @@ async def validate_user(message, button=None):
             error_msg = 'Token is expired, refresh your token and try again.'
             return error_msg, button
         
-        return None, None
+        return None
     
     except Exception as e:
         print(f"An error occurred in validate_user: {e}")
