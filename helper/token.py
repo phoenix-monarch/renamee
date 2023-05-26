c9adea2ac97a16f4649e5bddc5b62f8627d6ff05
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-26 11:17:14 +0530

    Update token.py

--- helper/token.py
+++ helper/token.py
@@ -14,8 +14,6 @@ async def none_admin_utils(message):
             error_msg.append(token_msg)
             error_button = button    
         return error_msg, error_button
-    except AttributeError:
-        raise AttributeError("The 'message.chat' object is None or doesn't have the 'write' attribute.")
     except Exception as e:
         print(f"An error occurred in none_admin_utils: {e}")
 
@@ -23,22 +21,31 @@ async def validate_user(message, button=None):
     try:
         if not Config.TOKEN_TIMEOUT:
             return None, button
+
+        if message.chat is None or not hasattr(message.chat, 'write'):
+            raise AttributeError("The 'message.chat' object is None or doesn't have the 'write' attribute.")
+
         userid = message.from_user.id
         data = await db.get_user_data(userid)
         expire = data.get('time')
-        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)    
+        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
+
         if is_expired:
             token = data.get('token') if expire is None and 'token' in data else str(uuid.uuid4())
             if expire is not None:
                 del data['time']
             data['token'] = token
-            await db.update_user_data(userid, data)        
+            await db.update_user_data(userid, data)
+
             if button is None:
-                button = InlineKeyboardButton(text='Refresh Token', url=shorten_url(f'https://t.me/{Config.BOT_NAME}?start={token}'))
-            
+                button = InlineKeyboardButton(
+                    text='Refresh Token',
+                    url=shorten_url(f'https://t.me/{Config.BOT_NAME}?start={token}')
+                )
+
             error_msg = 'Token is expired, refresh your token and try again.'
             return error_msg, button
-        
+
         return None, button
     except Exception as e:
         print(f"An error occurred in validate_user: {e}")
