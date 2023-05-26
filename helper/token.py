1091703c38cf6dacaeed347fcd68b990f7ed41eb
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-05-26 12:53:15 +0530

    Update token.py

--- helper/token.py
+++ helper/token.py
@@ -1,59 +1,46 @@
-import os, asyncio, uuid
+import os, asyncio, uuid, traceback
 from time import time
 from pyrogram.types import InlineKeyboardButton
 from helper.database import db
 from config import Config
 from shortener import shorten_url
 
-async def none_admin_utils(message):
+ async def none_admin_utils(message):
     try:
-        result = await validate_user(message)
         error_msg = []
         error_button = None
-        
-        if result and isinstance(result, tuple):
-            token_msg, button = result
-            if token_msg is not None:
-                error_msg.append(token_msg)
-                error_button = button
-    
+        token_msg, button = await validate_user(message)
+        if token_msg is not None:
+            error_msg.append(token_msg)
+            error_button = button    
         return error_msg, error_button
-
     except Exception as e:
         print(f"An error occurred in none_admin_utils: {e}")
+        traceback.print_exc()
+        return [], None
 
 async def validate_user(message, button=None):
     try:
         if not Config.TOKEN_TIMEOUT:
             return None, button
-
-        if message.chat is None or not hasattr(message.chat, 'write'):
-            error_msg = "The 'message.chat' object is None or doesn't have the 'write' attribute."
-            error_button = None
-            return error_msg, error_button
-
         userid = message.from_user.id
         data = await db.get_user_data(userid)
         expire = data.get('time')
-        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)
-
+        is_expired = (expire is None or (time() - expire) > Config.TOKEN_TIMEOUT)    
         if is_expired:
             token = data.get('token') if expire is None and 'token' in data else str(uuid.uuid4())
             if expire is not None:
                 del data['time']
             data['token'] = token
-            await db.update_user_data(userid, data)
-
+            await db.update_user_data(userid, data)        
             if button is None:
-                button = InlineKeyboardButton(
-                    text='Refresh Token',
-                    url=shorten_url(f'https://t.me/{Config.BOT_NAME}?start={token}')
-                )
-
+                button = InlineKeyboardButton(text='Refresh Token', url=shorten_url(f'https://t.me/{Config.BOT_NAME}?start={token}'))
+            
             error_msg = 'Token is expired, refresh your token and try again.'
             return error_msg, button
-
+        
         return None, button
-
     except Exception as e:
         print(f"An error occurred in validate_user: {e}")
+        traceback.print_exc()
+        return [], None
