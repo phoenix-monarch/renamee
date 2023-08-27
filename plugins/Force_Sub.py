83cbb2bb4a5dbd65c0e7a7edca668ca0f7cd3c63
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-08-28 01:57:22 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, enums 
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
@@ -8,25 +8,31 @@ async def not_subscribed(_, client, message):
     await db.add_user(client, message)
     if not Config.FORCE_SUB:
         return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
+    try:
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+        if user.status == "kicked":
+            return True
+        elif user.status == "restricted":
+            return False           
         else:
-            return False                
+            return False
     except UserNotParticipant:
-        pass
-    return True
-
+        return True
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
-async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
+async def force_sub(client, message):
     try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
-        if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
-    except UserNotParticipant:                       
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+        if not link:
+            await client.export_chat_invite_link(Config.FORCE_SUB)
+            link = (await user.chat.get_invite_link()).invite_link
+        invitelink = link
+        if user.status == "kicked":
+            await client.send_message(message.from_user.id, text="Sorry, You're Banned To Use Me")
+            return
+    except UserNotParticipant:
+        pass
+
+    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=invitelink)]]
+    text = "**Sorry Dude, You're Not Joined My Channel 😐. So Please Join Our Update Channel To Continue**"
+    await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
